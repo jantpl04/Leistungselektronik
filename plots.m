2063bbdfb67dd4db5fe3d651589794a76b58b186
Jan Tepel PC <et22091@lehre.dhbw-stuttgart.de>
2025-03-03 16:05:02 +0100

    .

--- plots.m
+++ plots.m
@@ -8,7 +8,7 @@ plot(out.m_v, 'Color', [0, 0.5, 0], 'LineWidth', 1.5);
 plot(out.m_w, 'Color', [0.5, 0, 0.5], 'LineWidth', 1.5);
 plot(out.m_0, 'r', 'LineWidth', 1.5);
 ylim([-1.2, 1.2]);
-
+legend('c', 'm_u','m_v','m_w', 'm_0');
 
 %%
 subplot(2,1,2);
@@ -16,7 +16,7 @@ plot(out.c, 'b', 'LineWidth', 0.8); hold on;
 plot(out.m_u0, 'Color', [0.8, 0.7, 0], 'LineWidth', 1.5);
 plot(out.m_v0, 'Color', [0, 0.5, 0], 'LineWidth', 1.5);
 plot(out.m_w0, 'Color', [0.5, 0, 0.5], 'LineWidth', 1.5);
-
+legend('c', 'm_{u0}','m_{v0}','m_{w0}');
 ylim([-1.2, 1.2]);
 
  
